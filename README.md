d246bebb39c9db6b6543dff6d54f31982e507960
Alejandro Flores <alejandro.flores.alecs@gmail.com>
2025-11-22 19:07:49 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Install dependencies
+```sh
+pip install -r requirements.txt
+```
+## Para poblar el dataset 
+```sh
+python3 scripts/videoreader.py --name alejandro
+```
+
+
+## Save dependencies 
+```sh
+pip freeze > requirements.txt
+```
+
+## Run project
+```sh
+python3 src/cnn.py
+```
---
+++ scripts/videoreader.py
@@ -0,0 +1,52 @@
+import argparse
+import cv2
+import os
+from pathlib import Path
+import sys
+
+argument_parser = argparse.ArgumentParser()
+
+argument_parser.add_argument(
+        '--name', default=None, type=str,
+        help='name of the dataset output')
+
+args = argument_parser.parse_args()
+
+if args.name == None:
+  exit()
+
+root_dir = Path(__file__).resolve().parent.parent
+
+output_folder = f"{root_dir}/dataset/{args.name}"
+
+if not os.path.exists(output_folder):
+  os.makedirs(output_folder)
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+  print("Error: Could not open video device")
+  exit()
+
+frame_count = 0
+while True:
+  ret, frame = cap.read()
+
+  if not ret:
+    print("Error: Failed to capture frame.")
+    break
+
+  cv2.imshow("Live Feed", frame)
+
+  key = cv2.waitKey(1) & 0xFF
+  if key == ord('s'):
+    image_filename = os.path.join(output_folder, f"image_{frame_count:04d}.jpg")
+    cv2.imwrite(image_filename, frame)
+    print(f"Image saved: {image_filename}")
+    frame_count += 1
+
+  elif key == ord('q'):
+    break
+
+cap.release()
+cv2.destroyAllWindows()
